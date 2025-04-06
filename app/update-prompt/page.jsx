e4fbca6b1434d59b9002cf2dfcb1d75540319b0b
suraj <sharadadde@gmail.com>
2025-04-06 16:08:44 +0530

    updated regarding suspend

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,9 +1,8 @@
-'use client' 
+'use client';
 
-import { useEffect, useState } from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter, useSearchParams } from 'next/navigation'
-import Form from '@components/Form'
+import React, { Suspense, useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import Form from '@components/Form';
 
 const EditPrompt = () => {
     const router = useRouter();
@@ -13,60 +12,58 @@ const EditPrompt = () => {
     const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({
         prompt: '',
-        tag: ''
+        tag: '',
     });
 
     useEffect(() => {
         const getPromptDetails = async () => {
-            const response = await fetch(`/api/prompt/${promptId}`)
+            const response = await fetch(`/api/prompt/${promptId}`);
             const data = await response.json();
 
             setPost({
                 prompt: data.prompt,
                 tag: data.tag,
-            })
-        }
+            });
+        };
 
-        if(promptId) getPromptDetails();
-    },[promptId])
+        if (promptId) getPromptDetails();
+    }, [promptId]);
 
     const updatePrompt = async (e) => {
         e.preventDefault();
         setSubmitting(true);
 
-        if(!promptId) return alert('Prompt ID not found')
+        if (!promptId) return alert('Prompt ID not found');
 
-        try{
-            const response = await fetch(`/api/prompt/${promptId}`,{
+        try {
+            const response = await fetch(`/api/prompt/${promptId}`, {
                 method: 'PATCH',
                 body: JSON.stringify({
                     prompt: post.prompt,
-                    tag: post.tag
-                })
-            })
-            if(response.ok){
+                    tag: post.tag,
+                }),
+            });
+            if (response.ok) {
                 router.push('/');
             }
-        }
-        catch(error){
+        } catch (error) {
             console.log(error);
-        }
-        finally{
+        } finally {
             setSubmitting(false);
         }
-    }
+    };
 
     return (
-        <Form
-            type="Edit"
-            post = {post}
-            setPost = {setPost}
-            submitting = {submitting}
-            handleSubmit = {updatePrompt}
-        />
-    )
-}
+        <Suspense fallback={<div>Loading...</div>}>
+            <Form
+                type="Edit"
+                post={post}
+                setPost={setPost}
+                submitting={submitting}
+                handleSubmit={updatePrompt}
+            />
+        </Suspense>
+    );
+};
 
-export default EditPrompt
-
-//? 01:52:09 -> video completed
+export default EditPrompt;
